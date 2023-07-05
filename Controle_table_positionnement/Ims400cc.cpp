163087bf2998fef0dd9fe8bc137c4e9410156cf7
Olivier Romain <olivierromain140@gmail.com>
2023-07-05 18:04:31 -0400

    Implémentation de la classe Ims400cc. Ce code n'est pas testé.

--- Controle_table_positionnement/Ims400cc.cpp
+++ Controle_table_positionnement/Ims400cc.cpp
@@ -1,36 +1,91 @@
+#include "Arduino.h"
 #include "Ims400cc.h"
 
-Ims400cc::Ims400cc(byte motorA_pin, byte motorB_pin, byte current_sens_pin, byte eor_p, byte eor_n, byte meca_0, byte led_pin){
+Ims400cc::Ims400cc(byte motorA_pin, byte motorB_pin, byte current_sens_pin, byte phaseA, byte phaseB, byte index_pulse, byte eor_p, byte eor_n, byte meca_0, byte led_pin, int pid_delay) : PosCounter(phaseA, phaseB, index_pulse){
   _motorA_pin = motorA_pin;
   _motorB_pin = motorB_pin;
+  pinMode(motorA_pin, OUTPUT);
+  pinMode(motorB_pin, OUTPUT);
+  digitalWrite(motorA_pin, LOW);
+  digitalWrite(motorB_pin, LOW);
+
   _current_sens_pin = current_sens_pin;
-
-  attach_limit_switch_pins(eor_p, eor_n, meca_0);
-  attach_moving_led_pin(led_pin);
-}
-
-void Ims400cc::init_pos_counter(byte phaseA, byte phaseB, byte index_pulse){
-  
-}
-
-void Ims400cc::init_pid(float p, float i, float d, int hist_length){
-
-}
-
-void Ims400cc::attach_limit_switch_pins(byte eor_p, byte eor_n, byte meca_0){
   _eor_p = eor_p;
   _eor_n = eor_n;
   _meca_0 = meca_0;
+  pinMode(current_sens_pin, INPUT);
+  pinMode(eor_p, INPUT);
+  pinMode(eor_n, INPUT);
+  pinMode(meca_0, INPUT);
+
+  _moving_led_pin = led_pin;
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
+
+  _pid_delay = pid_delay;
+
+  set_step_conversion(1);  // 1 micromètre de déplacement correspond à un changement de phase
+
+//  attach_limit_switch_pins(eor_p, eor_n, meca_0);
 }
 
-void Ims400cc::attach_moving_led_pin(byte pin){
-  _moving_led_pin = pin;
+// void Ims400cc::init_pos_counter(byte phaseA, byte phaseB, byte index_pulse){
+  
+// }
+
+void Ims400cc::init_pid(float p, float i, float d, int hist_length){
+  _PID = Pid(p, i, d, hist_length);
+}
+
+byte Ims400cc::get_interrupt_pin(char pin){
+  switch (pin){
+    case 'p': return _eor_p;
+    case 'n': return _eor_n;
+    case '0': return _meca_0;
+  }
+}
+
+void Ims400cc::stop_interruption(){
+  digitalWrite(_motorA_pin, HIGH);
+  digitalWrite(_motorB_pin, HIGH);
+  _stop_flag = true;
 }
 
 void Ims400cc::go_to(float position){
+  digitalWrite(_moving_led_pin, HIGH);
+  digitalWrite(_motorA_pin, HIGH);
+  digitalWrite(_motorB_pin, HIGH);
 
+  _PID.set_target(position);
+  int vitesse = 0;
+  float command;
+  
+  float signal = get_position(2);
+
+  while (((position - signal > 100) or (vitesse > 100)) and (not _stop_flag)){
+    command = _PID.command(signal);
+    if (command > 0){
+      analogWrite(_motorA_pin, (1-command) * 1275);  // 255 * 5 = 1275
+      digitalWrite(_motorB_pin, HIGH);
+    } else {
+      digitalWrite(_motorA_pin, HIGH);
+      analogWrite(_motorB_pin, (1+command) * 1275);
+    }
+
+    delayMicroseconds(_pid_delay);
+  }
+
+  digitalWrite(_motorA_pin, HIGH);
+  digitalWrite(_motorB_pin, HIGH);
+  delay(100);  // Pour laisser le temps à la table de positionnement de s'arrêter
+
+  digitalWrite(_moving_led_pin, LOW);
+  digitalWrite(_motorA_pin, LOW);
+  digitalWrite(_motorB_pin, LOW);
+
+  _stop_flag = false;
 }
 
-float Ims400cc::get_position(){
-  return _position;
-}
+// float Ims400cc::get_position(){
+//   return _position;
+// }
--- Controle_table_positionnement/Ims400cc.h
+++ Controle_table_positionnement/Ims400cc.h
@@ -3,11 +3,10 @@
 
 #include <Arduino.h>
 #include "Pid.h"
-#include "Pos_counter.h"
+#include "PosCounter.h"
 
-class Ims400cc{
+class Ims400cc : public PosCounter{
   private:
-    float _position;  // Position de la table
     byte _motorA_pin;  // Motor +
     byte _motorB_pin;  // Motor -
     byte _current_sens_pin;  // Miroir de courant du driver
@@ -16,19 +15,26 @@ class Ims400cc{
     byte _meca_0;  // Mecanical zero
     byte _moving_led_pin;  // LED indiquant un déplacement
     Pid _PID;  // PID
+    int _pid_delay;  // Délais en microsecondes entre chaque appel de commande du pid
+    bool _stop_flag = false;  // Mettre à true pour arrêter la table de positionnement si elle est à une de ses extrémités
 
   public:
-    Ims400cc() {};
-    Ims400cc(byte motorA_pin, byte motorB_pin, byte current_sens_pin, byte eor_p=0, byte eor_n=0, byte meca_0=0, byte led_pin=0);
+    Ims400cc(byte motorA_pin, byte motorB_pin, byte current_sens_pin, byte phaseA, byte phaseB, byte index_pulse, byte eor_p, byte eor_n, byte meca_0, byte led_pin, int pid_delay);
 
-    void init_pos_counter(byte phaseA, byte phaseB, byte index_pulse)
+//    void init_pos_counter(byte phaseA, byte phaseB, byte index_pulse)
     void init_pid(float p, float i, float d, int hist_length=10);
 
-    void attach_limit_switch_pins(byte eor_p, byte eor_n, byte meca_0);
-    void attach_moving_led_pin(byte pin);
+    byte get_interrupt_pin(char pin);
+    /*
+    'p': end of run positive
+    'n': end of run negative
+    '0': mecanical zero
+    */
+    void stop_interruption();  // Interruption à appeler lorsque la table de positionnement est à une extrémité (eor déclenché)
+    void zero_interrupt();  // Interruption à appeler lorsque le mecanical_zero est déclenché. Cette fonction n'a pas d'utilité pour l'instant et n'est donc pas définie
 
     void go_to(float position);
-    float get_position();
+//    float get_position();
 };
 
 #endif
