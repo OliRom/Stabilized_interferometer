b02a79e1ca48bde828a70f843693e58044a37585
Olivier Romain <olivierromain140@gmail.com>
2023-08-03 15:44:23 -0400

    Implémentation des méthodes pour envoyer et recevoir des commandes au Arduino et à Labview. Implémentation des méthodes pour ouvrir une connection avec le Arduino et un socket (pour Labview) et pour envoyer et recevoir des commandes. Implémentation des méthodes pour encoder les commandes à envoyer sous forme de bytes et pour les décodes. Modification des valeurs des paramètres du lockin. Autres améliorations mineures.

--- piezo_control.py
+++ piezo_control.py
@@ -3,6 +3,18 @@ import zhinst.toolkit as zh
 import numpy as np
 import tkinter as tk
 import threading
+import serial as seri
+from serial.tools.list_ports import comports
+import socket
+
+
+def get_arduino_port():
+    arduino_ports = list()
+    for port in comports():
+        if "Arduino Leonardo" in port.description:
+            arduino_ports.append(port)
+
+    return arduino_ports
 
 
 class StabilizedInterferometer:
@@ -24,16 +36,28 @@ class StabilizedInterferometer:
         session = zh.Session(host, hf2=True)
         self.device = session.connect_device(device_name)
 
-    def connect_arduino(self, port=None, name=None):
+    def connect_arduino(self, port=None):
         """
         Fonction qui scanne tous les ports de l'ordi pour trouver un Arduino. Les paramètres peuvent être utilisés
         pour faciliter ou cibler la recherche avec l'ordi.
         :param port: port de l'ordi sur lequel le Arduino est branché
-        :param name: nom du Arduino
         """
-        pass
 
-    def lkin_setup(self, pid_params=(10, 1, 1)):
+        if port is not None:
+            self.ardui = seri.Serial(port, 9600)
+        else:
+            self.ardui = seri.Serial(get_arduino_port()[0].name)
+
+    def connect_socket(self, host="127.0.0.1", port=65432):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((host, port))
+        self.sock.listen(1)
+        print("En attente d'une connection...")
+        self.sock_conn, client_address = self.sock.accept()
+        print(f"Connection établie avec: {client_address}")
+        self.sock_conn.settimeout(0.1)
+
+    def lkin_setup(self, pid_params=(0.075, 3, 1e-6)):
         """
         Fonction à appeler au début de la prise de mesures pour initialiser le lockin avec les paramètres à utiliser.
         :param pid_params: paramètres du pid (P, I, D)
@@ -41,14 +65,14 @@ class StabilizedInterferometer:
         # Génération du signal à 1kHz
         self.device.oscs[0].freq(1000)
         sig = self.device.sigouts[0]
-        sig.amplitudes(0.02)
+        sig.amplitudes(0.003)  # Il faut 0.002 pour avoir 0.02 V (???)
         sig.range(10)
         sig.offset(0.0)
         sig.add(True)
 
         # Réception du signal de la photodiode
         sig = self.device.sigins[0]
-        sig.range(1.2)
+        sig.range(2)
         sig.ac(False)
         sig.imp50(False)
         sig.diff(False)
@@ -56,8 +80,8 @@ class StabilizedInterferometer:
         # Setup des démodulateurs
         for i in range(2):
             demod = self.device.demods[i]
-            demod.oscselect(i)
-            demod.harmonic(i+1)
+            demod.oscselect(0)
+            demod.harmonic(i + 1)
 
         # Initialiser le PID
         pid = self.device.pids[0]
@@ -65,7 +89,8 @@ class StabilizedInterferometer:
         pid.inputchanne(0)
         pid.output(3)  # output = 3: sortie auxout
         pid.outputchannel(2)
-        pid.outputdefaultenable(False)
+        pid.outputdefaultenable(True)
+        pid.outputdefault(0)
         pid.p(pid_params[0])
         pid.i(pid_params[1])
         pid.d(pid_params[2])
@@ -80,7 +105,7 @@ class StabilizedInterferometer:
         auxs = self.device.auxouts
         for i in range(2):
             aux = auxs[i]
-            aux.outputselect(2)  # outputselect = 2: module du signal du démodulateur (R)
+            aux.outputselect(0)  # outputselect = 0: composante x du signal du démodulateur (X)
             aux.demodselect(i)
             aux.scale(1.0)
             aux.offset(0.0)
@@ -92,15 +117,15 @@ class StabilizedInterferometer:
         """
         Activation des canaux du lockin.
         """
-        self.device.sigouts[0].on(True)
-        self.device.pids[0].enable(True)
+        self.device.sigouts[0].on(1)
+        self.activate_pid()
 
-    def activate_pid(self, enable=True):
+    def activate_pid(self, enable=1):
         """
         Activation du PID du lockin.
         :param enable: True si le PID est activé
         """
-        pass
+        self.device.pids[0].enable(enable)
 
     def set_phase(self, phase):
         """
@@ -109,8 +134,8 @@ class StabilizedInterferometer:
         :param phase: phase (degrés)
         """
         auxs = self.device.auxouts
-        auxs[0].scale(self.k1 * np.cos(phase * np.pi / 180))
-        auxs[1].scale(self.k2 * np.sin(phase * np.pi / 180))
+        auxs[0].scale(self.k1 * 100 * np.cos(phase * np.pi / 180))
+        auxs[1].scale(self.k2 * 100 * np.sin(phase * np.pi / 180))
 
         self.phase = self.get_phase()
 
@@ -130,8 +155,8 @@ class StabilizedInterferometer:
         Définit la constante k1 pour la normalisation des signaux de démodulation.
         :param value: valeur de k1
         """
-        self.k1 = float(value)
-        self.set_phase(self.phase)
+        self.k1 = -float(value)
+        self.set_phase(45)
 
     def set_k2(self, value):
         """
@@ -139,7 +164,7 @@ class StabilizedInterferometer:
         :param value: valeur de k2
         """
         self.k2 = float(value)
-        self.set_phase(self.phase)
+        self.set_phase(45)
 
     def defile_franges(self):
         """
@@ -147,7 +172,7 @@ class StabilizedInterferometer:
         est reparti à son état initial. Il faut mettre l'attribut "self.frange_defilation" à False pour que les franges
         arrêtent de défiler.
         """
-        self.frange_defilation = False
+        self.frange_defilation = True
 
         aux = self.device.auxouts[2]
         offset = 0
@@ -155,17 +180,17 @@ class StabilizedInterferometer:
 
         pid = self.device.pids[0]
         pid_state = pid.enable()
-        pid.enable(False)
+        pid.enable(0)
 
         while self.frange_defilation:
             if increase:
-                offset += 0.01
+                offset += 0.005
             else:
-                offset -= 0.01
+                offset -= 0.005
 
             aux.offset(offset)
             pid.monitoroffset(offset)
-            time.sleep(0.01)
+            time.sleep(0.02)
 
             if offset >= 3:
                 increase = False
@@ -182,6 +207,7 @@ class StabilizedInterferometer:
         """
         # Création de la fenêtre principale
         window = tk.Tk()
+        window.geometry("400x250")
         window.title("Calibration")
         window.configure(bg="sky blue")  # Couleur d'arrière-plan
         window.iconbitmap("Anneaux_interference.ico")
@@ -200,7 +226,7 @@ class StabilizedInterferometer:
         text_label.grid(row=0, columnspan=2)
 
         # Curseur 1 (horizontal)
-        scale_x = tk.Scale(frame, from_=0.01, to=10, resolution=0.01, orient=tk.HORIZONTAL, command=self.set_k1,
+        scale_x = tk.Scale(frame, from_=0.01, to=20, resolution=0.05, orient=tk.HORIZONTAL, command=self.set_k1,
                            variable=self.k1, troughcolor="gold", bg="pale green", highlightthickness=0)
         scale_x.set(self.k1)
         scale_x.grid(row=1, column=0, padx=10)
@@ -209,7 +235,7 @@ class StabilizedInterferometer:
         label_x.grid(row=2, column=0)
 
         # Curseur 2 (vertical)
-        scale_y = tk.Scale(frame, from_=100, to=0.1, resolution=0.1, orient=tk.VERTICAL, command=self.set_k2,
+        scale_y = tk.Scale(frame, from_=200, to=0.1, resolution=0.5, orient=tk.VERTICAL, command=self.set_k2,
                            variable=self.k2, troughcolor="gold", bg="pale green", highlightthickness=0)
         scale_y.set(self.k2)
         scale_y.grid(row=1, column=1, padx=10)
@@ -240,10 +266,124 @@ class StabilizedInterferometer:
         # Lancement de la boucle principale de la fenêtre
         window.mainloop()
 
+    def go_to(self, pos, precision):
+        self.send_arduino_command(self.format_command("@", [pos, precision]))
+
+    def get_position(self):
+        self.send_arduino_command(self.format_command("?"))
+
+    def get_max_pos(self):
+        self.send_arduino_command(self.format_command("["))
+
+    def calibrate(self):
+        self.send_arduino_command(self.format_command("("))
+
+    def go_to_resp(self, pos):
+        self.send_socket_command(self.format_command(" ", [pos]))
+
+    def get_position_resp(self, pos):
+        self.send_socket_command(self.format_command("!", [pos]))
+
+    def get_max_pos_resp(self, pos):
+        self.send_socket_command(self.format_command("]", [pos]))
+
+    def calibrate_resp(self):
+        self.send_socket_command(self.format_command(")"))
+
+    def read_arduino_command(self, n=1):
+        for i in range(n):
+            if self.ardui.inWaiting() > 0:
+                code, args = self.decod_command(self.ardui.readline().decode("ascii"))
+
+                if code == "!":
+                    self.get_position_resp(*args)
+                elif code == " ":
+                    self.go_to_resp(*args)
+                elif code == "]":
+                    self.get_max_pos_resp(*args)
+                elif code == ")":
+                    self.calibrate_resp()
+
+    def read_socket_command(self, n=1):
+        for i in range(n):
+            try:
+                data = self.sock_conn.recv(1024).decode("ascii")
+                if data == "":
+                    print("Le socket a été déconnecté.")
+                    self.connect_socket()
+                    break
+
+                code, args = self.decod_command(data)
+
+                if code == "?":
+                    self.sock_conn.sendall("Message recu".encode("ascii"))
+                    self.get_position()
+                elif code == "@":
+                    self.go_to(*args)
+                elif code == "[":
+                    self.get_max_pos()
+                elif code == "(":
+                    self.calibrate()
+
+            except:
+                pass
+
+    def send_arduino_command(self, command):
+        self.ardui.write(command.encode("ascii"))
+
+    def send_socket_command(self, command):
+        self.sock_conn.sendall(command.encode("ascii"))
+
+    @staticmethod
+    def format_command(commande, args=()):
+        comm = "\x01" + commande
+        for a in args:
+            comm += str(a) + "\x2C"
+
+        if len(args) > 0:
+            return comm[:-1] + "\n"
+        else:
+            return comm + "\n"
+
+    @staticmethod
+    def decod_command(commande):
+        try:
+            commande = commande.strip()
+            commande = commande.partition(chr(0x01))[2]
+            code = commande[0]
+            args = commande[1:].split(",")
+
+            return code, args
+
+        except:
+            return None, None
+
 
 if __name__ == "__main__":
     syst = StabilizedInterferometer()
+    syst.k1, syst.k2 = 0.95, 56.5
     syst.connect_lkin("DEV415")
     syst.lkin_setup()
     syst.lkin_activation()
     syst.demod_calibration()
+    # syst.connect_arduino()
+    # syst.connect_socket()
+
+    # while True:
+    #     syst.read_socket_command()
+    #     syst.read_arduino_command()
+
+    # syst.calibrate()
+    # syst.get_position()
+    # syst.go_to(50000, 20)
+    # syst.get_max_pos()
+
+    # while True:
+    #     syst.read_arduino_command()
+
+    while True:
+        comm = input("Phase: ").strip()
+        if comm == "q":
+            break
+        syst.set_phase(float(comm))
+        print(syst.get_phase())
